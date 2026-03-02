ab29d9f89121c517bbe267e4da35877fa9569eb1
Kundan Kumar <126001733+kundankr4@users.noreply.github.com>
2026-03-01 21:28:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true }, // required for GitHub Pages with next/image
-  basePath: process.env.NODE_ENV === "production" ? "/kundan-portfolio": "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/kundan-portfolio/": "",
+  images: { unoptimized: true },
+  basePath: "/portfolio_website",
+  assetPrefix: "/portfolio_website/",
 };
 
 module.exports = nextConfig;
